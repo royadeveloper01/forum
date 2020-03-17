f48a3780545442556e85620f7b0649e8e6bba018
Pablo <royadeveloper01@gmail.com>
2020-03-17 18:18:32 +0000

    db chnages

--- config/database.php
+++ config/database.php
@@ -45,22 +45,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => 'db4free.net',
             'port' => '3306',
             'database' => 'forumapp',
             'username' => 'forumapp',
             'password' => 'app@forum123',
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'pgsql' => [
