9183585e6b5e886340eb6f5592227440d9e952a1
Pablo <royadeveloper01@gmail.com>
2020-03-19 17:02:33 +0000

    Social Auth

--- config/services.php
+++ config/services.php
@@ -30,4 +30,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id'     => '109087283051-krn8a68ab0s83rkee88t1usoodfrpn8i.apps.googleusercontent.com',
+        'client_secret' => 'ba7SE0QRBgxs4Gb26zmBlS2X',
+        'redirect'      => 'https://royalforum.herokuapp.com/google/callback',
+    ],
+
+    'github' => [
+        'client_id'     => '44aebcad6c09f6760119',
+        'client_secret' => '20dc2f7a4701b359cc0156fee7d35789d739406c',
+        'redirect'      => 'https://royalforum.herokuapp.com/github/callback',
+    ],
+
 ];
